42dc284ec58019b3a71d3778206096213ba98ec1
KSS675 <Olololoh568@tutamail.com>
2026-01-14 20:25:55 +0300

    Update OptionsBaseState.cpp

--- src/Menu/OptionsBaseState.cpp
+++ src/Menu/OptionsBaseState.cpp
@@ -246,9 +246,9 @@ void OptionsBaseState::btnOkClick(Action *)
 		if (Options::displayWidth != Options::newDisplayWidth ||
 			Options::displayHeight != Options::newDisplayHeight ||
 			Options::useOpenGL != Options::newOpenGL ||
-			Options::useNearestScaler != Options::newNearestScaler ||
-			Options::useLinearScaler != Options::newLinearScaler ||
-			Options::useAnisotropicScaler != Options::newAnisotropicScaler ||
+		 //	Options::useNearestScaler != Options::newNearestScaler ||
+	   	//	Options::useLinearScaler != Options::newLinearScaler ||
+		//	Options::useAnisotropicScaler != Options::newAnisotropicScaler ||
 			Options::useOpenGLShader != Options::newOpenGLShader)
 		{
 			_game->pushState(new OptionsConfirmState(_origin));
