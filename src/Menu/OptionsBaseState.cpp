626f6f483029dc39ba0bf612d7f0aa1693785385
KSS675 <Olololoh568@tutamail.com>
2026-01-14 19:38:01 +0300

    Update OptionsBaseState.cpp

--- src/Menu/OptionsBaseState.cpp
+++ src/Menu/OptionsBaseState.cpp
@@ -245,11 +245,11 @@ void OptionsBaseState::btnOkClick(Action *)
 		// Confirm any video options changes
 		if (Options::displayWidth != Options::newDisplayWidth ||
 			Options::displayHeight != Options::newDisplayHeight ||
-			Options::oneOpenGL != Options::newOpenGL ||
-			Options::oneNearestScaler != Options::newNearestScaler ||
-			Options::oneLinearScaler != Options::newLinearScaler ||
-			Options::oneAnisotropicScaler != Options::newAnisotropicScaler ||
-			Options::oneOpenGLShader != Options::newOpenGLShader)
+			Options::useOpenGL != Options::newOpenGL ||
+			Options::useNearestScaler != Options::newNearestScaler ||
+			Options::useLinearScaler != Options::newLinearScaler ||
+			Options::useAnisotropicScaler != Options::newAnisotropicScaler ||
+			Options::useOpenGLShader != Options::newOpenGLShader)
 		{
 			_game->pushState(new OptionsConfirmState(_origin));
 		}
